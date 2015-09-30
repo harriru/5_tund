54a32dd8bed802626a1280cac0fa6f7f1d914013
Harri Ruttas <harrrut@greeny.cs.tlu.ee>
2015-09-30 11:31:13 +0300

    esimene push

---
+++ data.php
@@ -0,0 +1,5 @@
+<?php
+	//data.php
+	// siia pääseb ligi sisseloginud kasutaja
+
+?>
---
+++ functions.php
@@ -0,0 +1,51 @@
+<?php 
+	
+	// Loon AB'i ühenduse
+	require_once("../../config_global.php");
+	$database = "if15_Harri_3";
+	
+	
+	// võtab andmed ja sisestab ab'i
+	// võtame vastu 2 muutujat
+	function createUser($create_email, $hash){
+		// Global muutujad, et kätte saada config failist andmed
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?,?)");
+		$stmt->bind_param("ss", $create_email, $hash);
+		$stmt->execute();
+		$stmt->close();
+		
+		$mysqli->close();
+		
+	}
+	
+	function loginUser($email, $hash){
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);		
+		
+		$stmt = $mysqli->prepare("SELECT id, email FROM user_sample WHERE email=? AND password=?");
+		$stmt = $mysqli->prepare("SELECT id, email FROM user_sample WHERE email=? AND password=?");
+		$stmt->bind_param("ss", $email, $hash);
+		$stmt->bind_result($id_from_db, $email_from_db);
+		$stmt->execute();
+		if($stmt->fetch()){
+			// ab'i oli midagi
+			echo "Email ja parool õiged, kasutaja id=".$id_from_db;
+		}else{
+			// ei leidnud
+			echo "Wrong credentials!";
+		}
+		$stmt->close();
+		
+		$mysqli->close();
+	}
+	
+	// fn sample
+	function hello($name, $age){
+		echo $name." ".$age;
+	}
+	
+	//hello("Romil", 5);
+	
+	
+?>
